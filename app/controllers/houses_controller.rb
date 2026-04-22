19aeb2711f8a6af57acb651f3a4dc073e0955a78
Tonny Donghwi Kim <tonny.kim@timberay.com>
2026-04-22 15:52:29 +0900

    feat(houses): add new/create flow with alias validation

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,5 +1,43 @@
 class HousesController < ApplicationController
+  before_action :set_house, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @houses = House.owned_by(owner_session_id).order(created_at: :desc)
+  end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(house_params.merge(owner_session_id: owner_session_id))
+    if @house.save
+      redirect_to @house
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_house
+    @house = House.owned_by(owner_session_id).find_by(id: params[:id])
+    head :not_found unless @house
+  end
+
+  def house_params
+    params.expect(house: [ :alias, :address, :agent_contact, :visited_at ])
   end
 end
--- test/controllers/houses_controller_test.rb
+++ test/controllers/houses_controller_test.rb
@@ -19,6 +19,29 @@ class HousesControllerTest < ActionDispatch::IntegrationTest
     refute_match "Someone else", @response.body
   end
 
+  test "GET /houses/new renders form" do
+    get new_house_path
+    assert_response :success
+    assert_select "form[action='#{houses_path}']"
+    assert_select "input[name='house[alias]']"
+  end
+
+  test "POST /houses creates house scoped to current session" do
+    get root_path # set cookie
+    assert_difference -> { House.count }, 1 do
+      post houses_path, params: { house: { alias: "신반포 32평", address: "서초구", visited_at: "2026-04-22" } }
+    end
+    h = House.last
+    assert_equal "신반포 32평", h.alias
+    assert h.owner_session_id.present?
+    assert_redirected_to house_path(h)
+  end
+
+  test "POST /houses rejects blank alias" do
+    post houses_path, params: { house: { alias: "" } }
+    assert_response :unprocessable_entity
+  end
+
   private
 
   def signed_cookie(name)
