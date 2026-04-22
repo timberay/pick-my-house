9fa11affd2d0b201293865000501f8258b87b468
Tonny Donghwi Kim <tonny.kim@timberay.com>
2026-04-22 16:26:42 +0900

    test(system): house deletion with confirmation dialog

---
+++ test/system/house_deletion_test.rb
@@ -0,0 +1,20 @@
+require "application_system_test_case"
+
+class HouseDeletionTest < ApplicationSystemTestCase
+  test "delete house from edit screen with confirmation" do
+    visit root_path
+    click_link "+ 새 집 추가"
+    fill_in "집 별칭 *", with: "사라질 집"
+    click_button "저장"
+
+    click_link "편집"
+
+    # confirm the turbo_confirm dialog
+    page.accept_confirm do
+      click_button "이 집 삭제"
+    end
+
+    assert_current_path root_path
+    assert_no_text "사라질 집"
+  end
+end
